3bd5b24bf3c4d5293d7759773678b14597c69819
esteban_1228 <juan.jaramillo.1228@gmail.com>
2024-05-18 04:51:10 -0500

    trabajo de tester

--- 01-suma/suma.test.js
+++
@@ -1,7 +0,0 @@
-
-const suma =require("./suma")
-
-test("adds 1 + 2 to equal 3", ()=>{
-    const result = suma(1,2);
-    expect (result).toBe(3);
-} )
--- 02-sub/sub.js
+++
@@ -1,8 +0,0 @@
-const sub = (num1, num2) => {
-    if (typeof num1 !== "number" || typeof num2 !== "number") {
-      return "los argumentos deben ser numeros enteros";
-    }
-    return Math.abs(num1 - num2);
-  };
-  
-  module.exports = sub;
